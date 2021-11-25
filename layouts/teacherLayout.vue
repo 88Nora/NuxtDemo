bbb5afff847fe211ff443eab10a5cb8ccbb41f48
Seantsang <9394wing@gmail.com>
2021-11-25 09:15:01 +0800

    udpate layout switch language function

--- layouts/teacherLayout.vue
+++ layouts/teacherLayout.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="Header">
-		<NuxtLink :to="$i18n.path('students')" class="Header_Link">{{$t('links.students')}}</NuxtLink>
-      	<NuxtLink :to="$i18n.path('books')" class="Header_Link">{{$t('links.books')}}</NuxtLink>
-		<NuxtLink :to="$i18n.path('')" class="Header_Link">{{$t('links.home')}}</NuxtLink>
+		<NuxtLink :to="localePath('students')" class="Header_Link">{{$t('links.students')}}</NuxtLink>
+      	<NuxtLink :to="localePath('books')" class="Header_Link">{{$t('links.books')}}</NuxtLink>
+		<NuxtLink :to="localePath('')" class="Header_Link">{{$t('links.home')}}</NuxtLink>
 		<NuxtLink v-if="$i18n.locale === 'en-US'" :to="`/zh-CN` + $route.fullPath" class="Header_Link" active-class="none" exact>
         	{{ $t('links.chinese') }}
       	</NuxtLink>
-      	<NuxtLink v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" class="Header_Link" active-class="none" exact>
+      	<NuxtLink v-else :to="switchLocalePath($i18n.locale === 'en' ? 'zh' : 'en')" class="Header_Link" active-class="none" exact>
         	{{ $t('links.english') }}
       	</NuxtLink>
 		<nuxt/>
